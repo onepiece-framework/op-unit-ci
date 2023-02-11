7793b87b385b485ea441dadd89074689525bd8f7
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-02-11 11:38:04 +0900

    Add: CI > CI_Class(), CI_Method(), CI_Args(), SaveCommitID(), CheckCommitID(), Display()

--- CI.class.php
+++ CI.class.php
@@ -140,4 +140,209 @@ class CI implements IF_UNIT
 		//	Save Commit ID.
 		self::SaveCommitID();
 	}
+
+	/** CI each Classes.
+	 *
+	 * @created    2023-02-10
+	 * @param      object      $obj
+	 */
+	static function CI_Class(object $obj)
+	{
+		//	You can specify and inspect onnly a specific method.
+		if( $method_list = OP()->Request('method') ){
+			$methods = [];
+			foreach( explode(',', $method_list) as $method_name ){
+				$methods[] = trim($method_name);
+			}
+		}else{
+			$methods = $obj->AllMethods();
+		}
+
+		//	Get CI Configs for that instance.
+		require_once(__DIR__.'/function/CIConfig.php');
+		$configs = CI\CIConfig($obj);
+
+		//	...
+		foreach( $methods as $method ){
+			//	Skip method
+			switch( $method ){
+				//	Magic method
+				case (strpos($method, '__') === 0 ) ? true: false;
+				case 'CI':
+				case 'AllMethods':
+				case 'Inspection':
+					continue 2;
+			}
+
+			//	Inspect each method.
+			self::CI_Method($obj, $method, $configs[$method] ?? [[]]);
+		}
+	}
+
+	/** CI Class each Methods.
+	 *
+	 * @created    2023-02-10
+	 * @param      object      $obj
+	 * @param      string      $method
+	 * @param      array       $configs
+	 * @throws \Exception
+	 */
+	static function CI_Method(object $obj, string $method, array $configs)
+	{
+		//	Inspect each args
+		foreach( $configs as $config ){
+			//	...
+			$expect = $config['result'] ?? null;
+			$arg    = $config['args']   ?? null;
+			$args   = is_array($arg) ? $arg: [$arg];
+			$result = null;
+			$traces = null;
+
+			//	Inspect each args
+			self::CI_Args($obj, $method, $args, $result, $traces);
+
+			//	If result is object.
+			if( is_object($result) ){
+				$result = get_class($result);
+			}
+
+			//	...
+			if( $result !== $expect ){
+				//	...
+				if( $traces ){
+					$i = count($traces);
+					echo "\n{$result}\n\n";
+					foreach( $traces as $trace){
+						$i--;
+						$n = str_pad((string)$i, 2, ' ', STR_PAD_LEFT);
+						echo "$n: ".OP()->DebugBacktraceToString($trace)."\n";
+					}
+					echo "\n";
+				}
+
+				//	...
+				echo "\n";
+				echo 'Config: ';
+				print_r($config);
+				echo "\n";
+				echo 'Expect: ';
+				print_r($expect);
+				echo "\n\n";
+				echo 'Result: ';
+				print_r($result);
+
+				//	...
+				$class = get_class($obj);
+				throw new \Exception("{$class}->{$method}(): Unmatch result.");
+			}
+		}
+	}
+
+	/** CI Class Method each Arguments.
+	 *
+	 * @created    2023-02-10
+	 * @param      object      $obj
+	 * @param      string      $method
+	 * @param      array       $args
+	 * @param      array       $result
+	 * @param      array       $traces
+	 */
+	static function CI_Args(object $obj, string $method, array $args, array &$result, array &$traces)
+	{
+		//	...
+		$traces = null;
+
+		//	...
+		try {
+			//	Inspection.
+			ob_start();
+			if(!$result = $obj->Inspection($method, ...$args) ){
+				//	If empty return value, evaluate contents.
+				if( $contents = ob_get_contents() ){
+					$result   = $contents;
+				}
+			}
+			ob_end_clean();
+
+			//	Overwrite result by Notice.
+			if( OP()->Notice()->Has() ){
+				$notice = OP()->Notice()->Pop();
+				$result = 'Notice: '.$notice['message'];
+				$traces = $notice['backtrace'];
+			}
+
+		}catch( \Exception $e ){
+			//	...
+			$result = 'Exception: '.$e->getMessage();
+			$traces = $e->getTrace();
+		}
+	}
+
+	/** Save inspected branch commit id.
+	 *
+	 * @created    2023-02-10
+	 */
+	static function SaveCommitID():void
+	{
+		//	...
+		$branch    = self::Git()->CurrentBranch();
+		$commit_id = self::Git()->CurrentCommitID();
+		$file_name = ".ci_commit_id_{$branch}";
+
+		//	...
+		file_put_contents($file_name, $commit_id);
+
+		//	...
+		self::Display("{$branch}:{$commit_id} --> {$file_name}");
+	}
+
+	/** Check if current commit id and saved commit id then already checked.
+	 *
+	 * @created    2023-02-10
+	 * @return     boolean
+	 */
+	static function CheckCommitID():bool
+	{
+		//	...
+		$branch    = self::Git()->CurrentBranch();
+		$commit_id = self::Git()->CurrentCommitID();
+		$file_name = ".ci_commit_id_{$branch}";
+		$saved_id  = file_get_contents($file_name);
+
+		//	...
+		$io = ($commit_id === $saved_id);
+
+		//	...
+		if( $io ){
+			self::Display("This branch is already inspected. ($branch)");
+		}
+
+		//	...
+		return $io;
+	}
+
+	/** Display message.
+	 *
+	 * @created    2023-02-10
+	 * @param      string      $message
+	 */
+	static function Display(string $message)
+	{
+		//	...
+		static $_display = null;
+
+		//	...
+		if( $_display === null ){
+			$_display = OP()->Request('display') ?? 1;
+		}
+
+		//	...
+		if(!$_display ){
+			return;
+		}
+
+		//	...
+		$current_dir = OP()->MetaPath()->Encode(getcwd());
+		echo "{$current_dir} - {$message}\n";
+	}
 }
