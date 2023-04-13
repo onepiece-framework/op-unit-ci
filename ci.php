be42b50fc6886d67f6e4286f75fd575f1b076d71
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-04-13 13:09:49 +0900

    Fix: ci.php | v2.2.0 - Inherit the PHP version of the execution source.

--- ci.php
+++ ci.php
@@ -2,13 +2,13 @@
 /** op-core:/ci.php
  *
  * @created    2023-01-01
- * @version    2.2.2
+ * @version    2.2.0
  * @package    op-core
  * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright  Tomoaki Nagahara All right reserved.
  */
 
- /** Declare strict
+/** Declare strict
  *
  */
 declare(strict_types=1);
@@ -18,7 +18,7 @@ declare(strict_types=1);
  */
 namespace OP;
 
-//	...
+//  Inherit the PHP version of the execution source.
 $php = $_SERVER['_'];
 
 //	...
