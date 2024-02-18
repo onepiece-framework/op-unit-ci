0e153a4b4a7cf313b7e3156fa72e9bb81053fa48
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-02-18 16:47:37 +0900

    New: function > GetSubmoduleConfig.php

---
+++ function/GetSubmoduleConfig.php
@@ -0,0 +1,45 @@
+<?php
+/** op-unit-ci:/function/GetSubmoduleConfig.php
+ *
+ * @created    2024-02-17
+ * @package    op-unit-ci
+ * @version    1.0
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\CI;
+
+/** Get gitsubmodule config.
+ *
+ * @return array
+ */
+function GetSubmoduleConfig() : array
+{
+	//	...
+	static $configs = [];
+
+	//	...
+	if( empty($configs) ){
+		//	...
+		$git_root = \OP\RootPath('git');
+
+		//	...
+		if( file_exists("{$git_root}.gitmodules") ){
+			$configs = OP()->Unit('Git')->SubmoduleConfig();
+		}else{
+			$configs = include(__DIR__.'/../include/GenerateSubmoduleConfig.php');
+		}
+	}
+
+	//	...
+	return $configs;
+}
