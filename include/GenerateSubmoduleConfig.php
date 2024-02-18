73c9385eb69281d4e0e817f1c2d2fc078755dc7c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-02-18 16:48:02 +0900

    New: include > GenerateSubmoduleConfig.php

---
+++ include/GenerateSubmoduleConfig.php
@@ -0,0 +1,47 @@
+<?php
+/** op-unit-ci:/include/GetSubmoduleConfig.php
+ *
+ * @created    2024-02-17
+ * @package    op-unit-ci
+ * @version    1.0
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\CI;
+
+//	...
+$configs = [];
+
+//	...
+$git_root   = \OP\RootPath('git');
+$asset_root = "{$git_root}asset";
+
+//	...
+$list = array_merge( glob("{$asset_root}/unit/*"),  glob("{$asset_root}/layout/*"), glob("{$asset_root}/module/*"), glob("{$asset_root}/webpack/*") );
+
+//	...
+foreach( $list as $path ){
+	//	...
+	if( strpos($path, $git_root) !== 0 ){
+		OP()->Notice("This path is not git root. ($path)");
+		continue;
+	}
+	//	...
+	$path = substr($path, strlen($git_root));
+	$name = basename($path);
+	$configs[$name] = [
+		'path' => $path,
+	];
+}
+
+//	...
+return $configs;
